ff52372759bb894ce2087759c10aae722a460275
Trushin Victor <v1996-96@mail.ru>
2016-04-24 21:19:31 +0300

    Animations and checks

--- js/App.js
+++ js/App.js
@@ -64,6 +64,51 @@ var App = (function ($) {
 		period : null  			// long_run | short_run
 	};
 
+	this.paramsFactory = {
+		checkAbilityToBuild : function( params ){
+			// Check object structure
+			if (typeof params.ecomonicsType == 'undefined' &&
+				typeof params.exchangeRate == 'undefined' &&
+				typeof params.economicsSize == 'undefined' &&
+				typeof params.capitalMobility == 'undefined' &&
+				typeof params.prices == 'undefined' &&
+				typeof params.period == 'undefined') 
+				throw { message: "Непредвиденная ошибка" };
+
+			// Check fields value
+			if (params.ecomonicsType == null ||
+				params.prices == null ||
+				params.period == null) 
+				throw { message: "Не все параметры определены. Не могу построить ((" };
+
+			if (params.ecomonicsType == "opened") {
+				if (params.exchangeRate == null ||
+					params.economicsSize == null ||
+					params.capitalMobility == null) 
+					throw { message: "Не все параметры определены. Не могу построить ((" };
+
+				if (!parent.useful.inArray(params.exchangeRate, ["fixed", "fluid"])) 
+					throw { message: "Непредвиденная ошибка" };	
+
+				if (!parent.useful.inArray(params.economicsSize, ["small", "big"])) 
+					throw { message: "Непредвиденная ошибка" };	
+
+				if (!parent.useful.inArray(params.capitalMobility, ["absolute", "high", "low", "null"])) 
+					throw { message: "Непредвиденная ошибка" };	
+			} else 
+				if (params.ecomonicsType !== "closed") 
+					throw { message: "Непредвиденная ошибка" };
+
+			if (!parent.useful.inArray(params.prices, ["fixed", "fluid"])) 
+				throw { message: "Непредвиденная ошибка" };
+
+			if (!parent.useful.inArray(params.period, ["long_run", "short_run"])) 
+				throw { message: "Непредвиденная ошибка" };
+
+			return true;
+		}
+	},
+
 	
 	/* Application functions */
 	/*-----------------------------------------------------------------*/
@@ -71,11 +116,14 @@ var App = (function ($) {
 		/* Prepare window for first use */
 		prepare : function(){
 			this.actions.interface = this;
+			this.actions.showSection( $("#paramsSection") );
 			this.actions.enableSorting();
 
 			this.handlers.interface = this;
 			this.handlers.modalHandler();
 			this.handlers.parametersHandler();
+			this.handlers.collapseParametersHandler();
+			this.handlers.buildHandler();
 		},
 
 
@@ -89,19 +137,22 @@ var App = (function ($) {
 		actions : {
 			interface : null,
 
+			/* Show sections */
+			showSection : function(section){
+				$(section).removeClass("hidden");
+				TweenLite.fromTo(
+					section, 1, 
+					{ opacity: 0, y: 60 },
+					{ opacity: 1, y: 0, ease: Power4.easeInOut }
+				);
+			},
+
 			/* Enable sorting in history list */
 			enableSorting : function(){
 				$("#historyPanels").sortable({
 	                handle: ".panel-sort-toggle"
 	            });
 	            $("#historyPanels").disableSelection();
-			},
-
-			/* Collapse list */
-			collapseList : function(list, collapse){
-				var collapse = collapse || false;
-				var action = collapse ? "show" : "hide";
-				$(list).collapse( action );
 			},
 
 			/* Toggle parameter */
@@ -119,6 +170,14 @@ var App = (function ($) {
 
 				var details = $(current).attr("data-details") || null;
 				if (details !== null) $(details).collapse("show");
+			},
+
+			/* Scroll to element */
+			scrollTo : function(element, topOffset){
+				var offset = $(element).offset().top - topOffset;
+				$('html, body').animate({
+                    scrollTop: offset
+                }, 160);
 			}
 		},
 
@@ -154,6 +213,41 @@ var App = (function ($) {
 					if (parent.params[ currentKey ] !== value) {
 						parent.params[ currentKey ] = value;
 						interface.actions.toggleParam(this);
+					}
+				});
+			},
+
+			/* Change icon when collapse parameters section */
+			collapseParametersHandler : function(){
+				$('#paramsList').on("show.bs.collapse", function(){
+					$('[data-toggle="collapse"][href="#paramsList"] i').html("keyboard_arrow_up");
+				});
+				$('#paramsList').on("hide.bs.collapse", function(){
+					$('[data-toggle="collapse"][href="#paramsList"] i').html("keyboard_arrow_down");
+				});
+			},
+
+			/* Handle click on build button */
+			buildHandler : function(){
+				$("#buildGraphsBtn").on("click", function(e){
+					e.preventDefault();
+
+					try{
+						parent.paramsFactory.checkAbilityToBuild( parent.params );
+
+						$("#paramsList").collapse("hide");
+
+						TweenLite.delayedCall(0.28, function(){
+							interface.actions.showSection( $("#graphsSection") );
+							interface.actions.showSection( $("#historySection") );
+						});
+
+						TweenLite.delayedCall(1.3, function(){
+							interface.actions.scrollTo( $("#graphsSection"), 15 );
+						});
+
+					} catch(e){
+						parent.message.show("Ошибка", e.message, "error");
 					}
 				});
 			}
