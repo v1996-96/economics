6db21ce361f5b236894873719e94c3f95d6402f9
Trushin Victor <v1996-96@mail.ru>
2016-04-25 00:43:22 +0300

    Enough for today

--- js/graphs/Base.js
+++ js/graphs/Base.js
@@ -5,6 +5,9 @@
 
 function Graph() {
 
+	/* Fields */
+	/*-----------------------------------------------------------------*/
+
 	/* To get ability to connect modules to graphs */
 	this.id = null;
 
@@ -14,17 +17,64 @@ function Graph() {
 	this.position.column = 0; // 0 to 3
 
 	/* Options for plugin */
-	this.title = "";
-	this.subTitle = "";
+	this.title = {};
+	this.title.text = "";
+	this.subTitle = {};
+	this.subTitle.text = "";
 
 	/* X axis */
 	this.xAxis = {};
 	this.xAxis.title = {};
 	this.xAxis.title.text = "xAxis";
+	this.xAxis.crosshair = true;
 
 	/* Y axis */
 	this.yAxis = {};
 	this.yAxis.title = {};
 	this.yAxis.title.text = "yAxis";
 
+	/* Series (graph data) */
+	this.series = [];
+
+
+	/* Methods */
+	/*-----------------------------------------------------------------*/
+	this.addLine = function( lineData ){
+		this.series.push(lineData);
+	}
+
+	this.calculate = {
+		axisData : function( min, max, interval ){
+			var data = [];
+			for (var i = min; i <= max; i += interval) 
+				data.push(i);
+			return data;
+		},
+
+		dataset : function( equation, axisData ){
+			var dataset = [];
+			for (var value in axisData) {
+				dataset.push( equation(value) );
+			}
+			return dataset;
+		},
+
+		seriesData : function( axisData, dataset ){
+			var series = Highcharts.map(dataset, function (val, j) {
+                return [axisData[j], val];
+            });
+
+            return series;
+		},
+
+		seriesFullData : function( equation, min, max, interval ){
+			var axisData = this.axisData( min, max, interval );
+			var dataset = this.dataset( equation, axisData );
+			var series = Highcharts.map(dataset, function (val, j) {
+                return [axisData[j], val];
+            });
+
+            return series;
+		}
+	};
 }
--- js/graphs/islmbp.js
+++ js/graphs/islmbp.js
@@ -11,7 +11,30 @@ islmbp.id = "islmbp";
 islmbp.position.row = 0;
 islmbp.position.column = 0;
 
+var axisData = islmbp.calculate.axisData( 0, 10, 0.1 );
 
+
+var dataset1 = islmbp.calculate.dataset( function(x){ return 40*x - 800; }, axisData );
+var seriesData1 = islmbp.calculate.seriesData( axisData, dataset1 );
+islmbp.addLine( {
+	name: "my_line",
+	type: "line",
+	marker: {
+		enabled: false
+	},
+	data: seriesData1
+} );
+
+var dataset2 = islmbp.calculate.dataset( function(x){ return -40*x + 250; }, axisData );
+var seriesData2 = islmbp.calculate.seriesData( axisData, dataset2 );
+islmbp.addLine( {
+	name: "my_line",
+	type: "line",
+	marker: {
+		enabled: false
+	},
+	data: seriesData2
+} );
 
 
 // Add graph to graph list
