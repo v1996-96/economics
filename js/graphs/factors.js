7d5aeca9326391a2218895c53d1da3f81751b77e
LeonidSmelov <sleonids@mail.ru>
2016-05-09 16:02:41 +0300

    Factors

--- js/graphs/factors.js
+++ js/graphs/factors.js
@@ -8,14 +8,57 @@ App.onBuild(function (params) {
 	console.log( params );
 
 	// Set default factors
-	
-	App.factors.setGlobal("G", 50);
-	App.factors.setGlobal("Xn", 0);
-	App.factors.setGlobal("T", 50);
-	App.factors.setGlobal("I", 50);
-	App.factors.setGlobal("MPC", 50);
-	App.factors.setGlobal("Ir", 50);
-	App.factors.setGlobal("Ia", 50);
-	App.factors.setGlobal("Ca", 50);
+	if(params.ecomonicsType == 'closed')
+	{
+		if(params.prices == 'fixed') 
+		{
+				App.factors.setGlobal("Xn", 0);
+				App.factors.setGlobal("MPM", 0);	
+				App.factors.setGlobal("G", 50);
+				App.factors.setGlobal("T", 50);
+				App.factors.setGlobal("I", 50);
+				App.factors.setGlobal("MPC", 50);
+				App.factors.setGlobal("Ir", 50);
+				App.factors.setGlobal("Ia", 50);
+				App.factors.setGlobal("Ca", 50);
+				App.factors.setGlobal("M/Pd", 50);
+				App.factors.setGlobal("M/Ps", 50);
+				App.factors.setGlobal("my", 50);
+				App.factors.setGlobal("mr", 50);
+		}
+		else 
+		{
+				App.factors.setGlobal("Xn", 0);	
+				App.factors.setGlobal("G", 50);
+				App.factors.setGlobal("T", 50);
+				App.factors.setGlobal("I", 50);
+				App.factors.setGlobal("MPC", 50);
+				App.factors.setGlobal("Ir", 50);
+				App.factors.setGlobal("Ia", 50);
+				App.factors.setGlobal("Ca", 50);
+				App.factors.setGlobal("M/Pd", 50);
+				App.factors.setGlobal("M/Ps", 50);
+				App.factors.setGlobal("my", 50);
+				App.factors.setGlobal("mr", 50);
+		}
+			
+	}
+	else 
+	{
+			App.factors.setGlobal("Xn", 50);
+				App.factors.setGlobal("MPM", 2);	
+				App.factors.setGlobal("G", 50);
+				App.factors.setGlobal("T", 50);
+				App.factors.setGlobal("I", 50);
+				App.factors.setGlobal("MPC", 50);
+				App.factors.setGlobal("Ir", 50);
+				App.factors.setGlobal("Ia", 50);
+				App.factors.setGlobal("Ca", 50);
+				App.factors.setGlobal("M/Pd", 50);
+				App.factors.setGlobal("M/Ps", 50);
+				App.factors.setGlobal("my", 50);
+				App.factors.setGlobal("mr", 50);
+
+	}
 
 });
