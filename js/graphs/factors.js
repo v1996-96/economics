83136f31015ed56b9db3a5ceed722debba9cf585
LeonidSmelov <sleonids@mail.ru>
2016-05-09 15:37:00 +0300

    Factors

--- js/graphs/factors.js
+++ js/graphs/factors.js
@@ -8,7 +8,14 @@ App.onBuild(function (params) {
 	console.log( params );
 
 	// Set default factors
+	
 	App.factors.setGlobal("G", 50);
-	App.factors.setGlobal("Xn", 5);
+	App.factors.setGlobal("Xn", 0);
+	App.factors.setGlobal("T", 50);
+	App.factors.setGlobal("I", 50);
+	App.factors.setGlobal("MPC", 50);
+	App.factors.setGlobal("Ir", 50);
+	App.factors.setGlobal("Ia", 50);
+	App.factors.setGlobal("Ca", 50);
 
 });
