cb1c8384dae95f08f29a541a434b2078017b7aa7
Trushin Victor <v1996-96@mail.ru>
2016-06-08 17:04:45 +0300

    how to write delta

--- js/modules/delta_G_equals_delta_b.js
+++ js/modules/delta_G_equals_delta_b.js
@@ -4,7 +4,7 @@ var g_equals_b = new Module();
 
 g_equals_b.id = "delta_G_equals_delta_b";
 
-g_equals_b.title = "&Delta;G = &Delta;b";
+g_equals_b.title = unescape("%u0394") + "G = "+ unescape("%u0394") +"b";
 g_equals_b.description = "Sample description";
 
 g_equals_b.action = function (graphs) {
