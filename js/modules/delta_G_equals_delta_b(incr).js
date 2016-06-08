b8403e57e97ef097c17deaaf4287a59f66353e52
Trushin Victor <v1996-96@mail.ru>
2016-06-08 18:35:05 +0300

    delta_G_equals_delta_b

---
+++ js/modules/delta_G_equals_delta_b(incr).js
@@ -0,0 +1,54 @@
+
+
+var g_equals_b = new Module();
+
+g_equals_b.id = "delta_G_equals_delta_b";
+
+g_equals_b.title = unescape("%u0394") + "G = "+ unescape("%u0394") +"b (incr)";
+g_equals_b.description = "Sample description";
+
+g_equals_b.action = function (graphs) {
+
+/* IS copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "islmbp", "is", "prevIs",
+		{ name : "Первоначальный IS" } );
+
+
+/* BP copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "islmbp", "bp", "prevBp",
+		{ name : "Первоначальный BP" } );
+
+
+/* AD copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "adas", "ad", "prevAd",
+		{ name : "Первоначальный AD" } );
+
+
+/* Md copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "moneyMarket", "moneyDemand", "prevMoneyDemand",
+		{ name : "Первоначальный Md" } );
+
+
+/* factExpenditure copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "keynesCross", "factExpenditure", "prevFactExpenditure",
+		{ name : "Первоначальный factExpenditure" } );
+
+	
+
+	/* Move line to new position */
+	var currentG = App.factors.current.get("G");
+	App.factors.current.set("G", currentG + 150);
+
+}
+
+App.module.add( g_equals_b );
