133d2d5fff9bc79152b4f482341852b925c6a5c5
pavlov sergey <cap.obvios@gmail.com>
2016-06-08 19:22:19 +0300

    Delta G delta T modules

---
+++ js/modules/delta_G_equals_delta_T(decr).js
@@ -0,0 +1,56 @@
+
+
+var g_equals_t_decr = new Module();
+
+g_equals_t_decr.id = "delta_G_equals_delta_T_decr";
+
+g_equals_t_decr.title = unescape("%u0394") + "G = "+ unescape("%u0394") +"T(decr)";
+g_equals_t_decr.description = "Sample description";
+
+g_equals_t_decr.action = function (graphs) {
+
+/* IS copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "islmbp", "is", "prevIs",
+		{ name : "Первоначальный IS" } );
+
+
+/* BP copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "islmbp", "bp", "prevBp",
+		{ name : "Первоначальный BP" } );
+
+
+/* AD copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "adas", "ad", "prevAd",
+		{ name : "Первоначальный AD" } );
+
+
+/* Md copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "moneyMarket", "moneyDemand", "prevMoneyDemand",
+		{ name : "Первоначальный Md" } );
+
+
+/* factExpenditure copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "keynesCross", "factExpenditure", "prevFactExpenditure",
+		{ name : "Первоначальный factExpenditure" } );
+
+	
+
+	/* Move line to new position */
+	var currentG = App.factors.current.get("G");
+	App.factors.current.set("G", currentG - 150);
+	var currentT = App.factors.current.get("T0");
+	App.factors.current.set("T0", currentT - 150);
+
+}
+
+App.module.add( g_equals_t_decr );
---
+++ js/modules/delta_G_equals_delta_T(incr).js
@@ -0,0 +1,56 @@
+
+
+var g_equals_t_incr = new Module();
+
+g_equals_t_incr.id = "delta_G_equals_delta_T_incr";
+
+g_equals_t_incr.title = unescape("%u0394") + "G = "+ unescape("%u0394") +"T(incr)";
+g_equals_t_incr.description = "Sample description";
+
+g_equals_t_incr.action = function (graphs) {
+
+/* IS copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "islmbp", "is", "prevIs",
+		{ name : "Первоначальный IS" } );
+
+
+/* BP copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "islmbp", "bp", "prevBp",
+		{ name : "Первоначальный BP" } );
+
+
+/* AD copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "adas", "ad", "prevAd",
+		{ name : "Первоначальный AD" } );
+
+
+/* Md copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "moneyMarket", "moneyDemand", "prevMoneyDemand",
+		{ name : "Первоначальный Md" } );
+
+
+/* factExpenditure copy
+--------------------------------------------------------*/
+	CapturePreviousLineState(
+		graphs, "keynesCross", "factExpenditure", "prevFactExpenditure",
+		{ name : "Первоначальный factExpenditure" } );
+
+	
+
+	/* Move line to new position */
+	var currentG = App.factors.current.get("G");
+	App.factors.current.set("G", currentG + 150);
+	var currentT = App.factors.current.get("T0");
+	App.factors.current.set("T0", currentT + 150);
+
+}
+
+App.module.add( g_equals_t_incr );
